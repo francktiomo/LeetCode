ab0658c481b4bf6941f137c50432d6c93c91b0a3
Franck Tiomo <einstein-franck.tiomo-epongo@polymtl.ca>
2025-03-12 23:46:45 -0400

    Add link to Leetcode problem

---
+++ Easy/2744.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/find-maximum-number-of-string-pairs/description/
+
+from typing import List
+
+class Solution:
+    def maximumNumberOfStringPairs(self, words: List[str]) -> int:
+        numOfPairs = 0
+        N = len(words)
+        for i in range(N):
+            for j in range(i+1, N):
+                if words[i] == words[j][::-1]:
+                    numOfPairs += 1
+
+        return numOfPairs
+
+if __name__ == '__main__':
+    solution = Solution()
+    assert solution.maximumNumberOfStringPairs(["cd","ac","dc","ca","zz"]) == 2
+    assert solution.maximumNumberOfStringPairs(["ab","ba","cc"]) == 1
+    assert solution.maximumNumberOfStringPairs(["aa","ab"]) == 0
