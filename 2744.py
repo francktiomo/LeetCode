df3f98612b16fa46d2fe13a7c9db990d5b30c916
Franck Tiomo <einstein-franck.tiomo-epongo@polymtl.ca>
2025-03-12 23:44:31 -0400

    Fix error

--- 2744.py
+++ 2744.py
@@ -3,8 +3,9 @@ from typing import List
 class Solution:
     def maximumNumberOfStringPairs(self, words: List[str]) -> int:
         numOfPairs = 0
-        for i in range(len(words)):
-            for j in range(i, len(words)):
+        N = len(words)
+        for i in range(N):
+            for j in range(i+1, N):
                 if words[i] == words[j][::-1]:
                     numOfPairs += 1
 
@@ -12,7 +13,6 @@ class Solution:
 
 if __name__ == '__main__':
     solution = Solution()
-    
-    # assert solution.maximumNumberOfStringPairs(["cd","ac","dc","ca","zz"]) == 2
-    # assert solution.maximumNumberOfStringPairs(["ab","ba","cc"]) == 1
-    # assert solution.maximumNumberOfStringPairs(["aa","ab"]) == 0
+    assert solution.maximumNumberOfStringPairs(["cd","ac","dc","ca","zz"]) == 2
+    assert solution.maximumNumberOfStringPairs(["ab","ba","cc"]) == 1
+    assert solution.maximumNumberOfStringPairs(["aa","ab"]) == 0
